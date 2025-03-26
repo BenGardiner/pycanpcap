d756cfb208a656f827a16b82c755d620fb465bfd
Ben Gardiner <ben.l.gardiner@gmail.com>
2025-03-26 12:54:51 -0400

    add file-input of a log file as an option to pycanpcap.log

--- pycanpcap/log.py
+++ pycanpcap/log.py
@@ -1,17 +1,36 @@
 import argparse
 import re
-
+import sys
+import signal
+import threading
+import time
 from ast import literal_eval
 
-from scapy.all import sniff, wrpcap
-import scapy.libs.six as six
+import can
+
 from scapy.config import conf
+from scapy.layers.can import CandumpReader
+
+try:
+    import scapy.libs.six as six
+
+    if six.PY2:
+        conf.contribs["CANSocket"] = {"use-python-can": True}
+except ModuleNotFoundError:
+    pass
 from scapy.consts import LINUX
 
-if six.PY2 or not LINUX or conf.use_pypy:
+if not LINUX or conf.use_pypy:
     conf.contribs["CANSocket"] = {"use-python-can": True}
 
+from scapy.scapypipes import WiresharkSink, WrpcapSink, SniffSource
+from scapy.sendrecv import sniff, AsyncSniffer
 from scapy.contrib.cansocket import CANSocket, PYTHON_CAN  # noqa: E402
+from scapy.contrib.isotp import ISOTPSocket
+
+from scapy.contrib.automotive.uds import UDS
+
+from scapy.pipetool import CLIFeeder, ConsoleSink, PipeEngine, Sink
 
 
 def create_socket(python_can_args, interface, channel, bitrate):
@@ -72,7 +91,7 @@ def main():
         "-c",
         "--channel",
         type=str,
-        required=True,
+        required=False,
         help="Additional arguments for a python-can Bus object.",
     )
     parser.add_argument(
@@ -93,24 +112,66 @@ def main():
     parser.add_argument(
         "-w", "--write", type=str, required=False, help="Output pcap file"
     )
+    parser.add_argument(
+        "-q", "--silent", type=bool, required=False, default=False, help="Silent mode"
+    )
+    parser.add_argument(
+        "-f", "--file", type=str, required=False, help="read from a candump instead"
+    )
 
     args = parser.parse_args()
 
-    sock, interface_string = create_socket(
-        args.python_can_args, args.interface, args.channel, args.bitrate
-    )
+    if args.file is not None:
+        sock = CandumpReader(args.file)
+        interface_string = "log"
+    else:
+        sock, interface_string = create_socket(
+            args.python_can_args, args.interface, args.channel, args.bitrate
+        )
 
     pretty_iface_name = sanitize_string(interface_string)
 
-    def packet_handler(pkt):
-        print(
-            f"({pkt.time:010.06f}) {pretty_iface_name} {pkt.identifier:03x}#{pkt.data.hex()}"
-        )
-        if args.write is not None:
-            wrpcap(args.write, pkt, append=True)
+    class PrintSink(Sink):
+        def push(self, pkt):
+            print(
+                f"({pkt.time:010.06f}) {pretty_iface_name} {pkt.identifier:03x}#{pkt.data.hex()}"
+            )
+            return
 
-    sniff(opened_socket=sock, prn=packet_handler)
+        def high_push(self, msg):
+            return
 
+    packet_count = 0
+    feeder = CLIFeeder()
+    if not args.silent:
+        printer = PrintSink()
+        feeder > printer
+    if args.write is not None:
+        wire = WrpcapSink(args.write)
+        feeder > wire
+
+    p = PipeEngine(feeder)
+    p.start()
+
+    def just_feed(p):
+        nonlocal packet_count
+        if not args.silent:
+            feeder.send(p)
+        packet_count = packet_count + 1
+        return
+    sniff(opened_socket=sock, prn=just_feed, store=0)
+
+    sock.close()
+    ## scapy / python-can BUG WORKAROUND: the end of the `with` block above .close()es the `csock2` which also deletes and
+    # closes the underlying python-can interface. If we don't monkey-patch the `low_csock.closed` then the result will be a
+    # deep stacktrace while python is shutting down later
+    sock.closed = True
+    sock.can_iface._is_shutdown = True
+
+    p.stop()
+    p.wait_and_stop()
+
+    sys.stderr.write("processed %d packets\n" % packet_count)
 
 if __name__ == "__main__":
     main()
